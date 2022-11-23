ce53e405cb53c3ebc8333b5ea764d302ca582288
Parker Harris <rparkerharris@gmail.com>
2022-11-22 17:28:36 -0800

    Test regular API

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("hello world");
+console.log("hello world"),fetch("https://graffiti.red/API",{method:"POST",body:JSON.stringify({action:"createUser",username:"poop",pass:"password123"})}).then((o=>{console.log("**********************"),console.log(o),console.log("**********************"),o.json().then((o=>{console.log("---------------------"),console.log(o),console.log("---------------------")})).catch((o=>{console.log("ERROR WITH JSON STEP"),console.log(o.message)}))})).catch((o=>{console.log("ERROR"),console.log(o.message)}));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log("hello world");
+
+fetch("https://graffiti.red/API", {
+    method: "POST",
+    body: JSON.stringify({
+        action: "createUser",
+        username: "poop",
+        pass: "password123"
+    })
+}).then((res) => {
+
+    console.log("**********************");
+    console.log(res);
+    console.log("**********************");
+
+    res.json().then((newRes) => {
+        console.log("---------------------");
+        console.log(newRes);
+        console.log("---------------------");
+    }).catch((err) => {
+        console.log("ERROR WITH JSON STEP");
+        console.log(err.message);
+    });
+}).catch((err) => {
+    console.log("ERROR");
+    console.log(err.message);
+});
