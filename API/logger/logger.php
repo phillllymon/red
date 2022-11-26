e54141b6bc0dfa32bbd0521ce39e45b96c393198
Parker Harris <rparkerharris@gmail.com>
2022-11-26 09:30:21 -0800

    auto dev

--- API/logger/logger.php
+++ API/logger/logger.php
@@ -4,6 +4,7 @@ function logIncomingRequest($requestData, $filePath) {
     $logFile = fopen($filePath, "a") or die();
     fwrite($logFile, "-------------------------------------------------\n");
     fwrite($logFile, $requestData);
+    fclose($logFile);
 }
 
 ?>
