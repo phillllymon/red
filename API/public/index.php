69888594789a937197c1db38e91ea511962fe060
Parker Harris <rparkerharris@gmail.com>
2022-11-26 09:38:18 -0800

    auto dev

--- API/public/index.php
+++ API/public/index.php
@@ -7,6 +7,7 @@ require("../logger/logger.php");
 
 $logFile = "../logs/log_public.txt";
 logIncomingRequest(file_get_contents('php://input'), $logFile);
+logIncomingRequest(json_encode($_SERVER), $logFile);
 
 
 $hostname = "localhost";
