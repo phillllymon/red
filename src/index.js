5960ab8d08e72da8add1ac5829a505f2d2e95427
Parker Harris <rparkerharris@gmail.com>
2022-11-29 16:24:28 -0800

    auto dev

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 console.log("hello world");
 
+fetch("https://graffiti.red/API/", {
+    method: "POST",
+    body: JSON.stringify({
+        action: "signUp",
+        username: "MrMagoo",
+        pass: "myFancyPassword",
+    })
+}).then((res) => {
+
+    console.log("**********************");
+    console.log(res);
+    console.log("**********************");
+
+    res.json().then((newRes) => {
+        console.log("---------------------");
+        console.log(newRes);
+        console.log("---------------------");
+    }).catch((err) => {
+        console.log("ERROR WITH JSON STEP");
+        console.log(err.message);
+    });
+}).catch((err) => {
+    console.log("ERROR");
+    console.log(err.message);
+});
+
 // fetch("https://graffiti.red/API/public/", {
 // // fetch("http://localhost:8000/API/public/", {
 //     method: "POST",
@@ -26,32 +52,5 @@ console.log("hello world");
 //     console.log("ERROR");
 //     console.log(err.message);
 // });
-
-fetch("https://localhost:8000/API/public/", {
-    type: "no-cors",
-    method: "POST",
-    body: JSON.stringify({
-        action: "signUp",
-        email: "poop@face.com",
-        feedback: "I love this friggin' thing!",
-    })
-}).then((res) => {
-
-    console.log("**********************");
-    console.log(res);
-    console.log("**********************");
-
-    res.json().then((newRes) => {
-        console.log("---------------------");
-        console.log(newRes);
-        console.log("---------------------");
-    }).catch((err) => {
-        console.log("ERROR WITH JSON STEP");
-        console.log(err.message);
-    });
-}).catch((err) => {
-    console.log("ERROR");
-    console.log(err.message);
-});
 
 console.log("Well....here I am.");
