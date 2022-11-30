2ae1a3782b7f7cc422d4367072e6bd70360d6e30
Parker Harris <rparkerharris@gmail.com>
2022-11-29 18:08:44 -0800

    auto dev

--- API/actions/createPost.php
+++ API/actions/createPost.php
@@ -45,7 +45,7 @@ function createPost($connection, $inputs) {
     try {
         $queryObj = $connection->prepare($getStatement);
         $queryObj->execute([$newPostId]);
-        $newPost = $queryObj->fetchAll();
+        $newPost = $queryObj->fetchAll()[0];
     } catch (PDOException $pe) {
         return setErrorReply("new post could not be retrieved");
     }
