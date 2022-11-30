55bbc1df0bebbdb39d13dd8b8ce80b0a6a232920
Parker Harris <rparkerharris@gmail.com>
2022-11-29 17:26:54 -0800

    auto dev

--- API/actions/logOut.php
+++ API/actions/logOut.php
@@ -5,39 +5,34 @@ function logOut($connection, $inputs) {
 
     $reply = new stdClass();
 
-    // TEST ONLY
-    $reply->message = "testing here F";
+    if (!checkForData($inputs, ["username", "token"])) {
+        return setErrorReply("username and token required");
+    }
+
+    $getStatement = "SELECT * FROM users WHERE username=?";
+    try {
+        $queryObj = $connection->prepare($getStatement);
+        $queryObj->execute([$inputs->username]);
+        $existingUsers = $queryObj->fetchAll();
+    } catch (PDOException $pe) {
+        return setErrorReply("database error");
+    }
+
+    if (count($existingUsers) != 1) {
+        return setErrorReply("user not found");
+    }
+
+    $updateStatement = "UPDATE users SET token=? WHERE username=?";
+    try {
+        $queryObj = $connection->prepare($updateStatement);
+        $queryObj->execute([null, $inputs->username]);
+        $reply->status = "success";
+        $reply->message = "user logged out";
+    } catch (PDOException $pe) {
+        return setErrorReply("database error");
+    }
+
     return $reply;
-    // END TEST
-
-    // if (!checkForData($inputs, ["username", "token"])) {
-    //     return setErrorReply("username and token required");
-    // }
-
-    // $getStatement = "SELECT * FROM users WHERE username=?";
-    // try {
-    //     $queryObj = $connection->prepare($getStatement);
-    //     $queryObj->execute([$inputs->username]);
-    //     $existingUsers = $queryObj->fetchAll();
-    // } catch (PDOException $pe) {
-    //     return setErrorReply("database error");
-    // }
-
-    // if (count($existingUsers) != 1) {
-    //     return setErrorReply("user not found");
-    // }
-
-    // $updateStatement = "UPDATE users SET token=? WHERE username=?";
-    // try {
-    //     $queryObj = $connection->prepare($updateStatement);
-    //     $queryObj->execute([null, $inputs->username]);
-    //     $reply->status = "success";
-    //     $reply->message = "user logged out";
-    // } catch (PDOException $pe) {
-    //     return setErrorReply("database error");
-    // }
-
-    // return $reply;
 }
 
 ?>
