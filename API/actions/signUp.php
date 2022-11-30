216a6a675fd73389a6eedce687f8eb012c66645a
Parker Harris <rparkerharris@gmail.com>
2022-11-29 16:27:43 -0800

    auto dev

--- API/actions/signUp.php
+++ API/actions/signUp.php
@@ -6,6 +6,11 @@ function signUp($connection, $inputs) {
 
     $reply = new stdClass();
 
+    // TEST ONLY
+    $reply->temp = "testing";
+    return $reply;
+    // END TEST
+
     if (!checkForData($inputs, ["username", "pass"])) {
         return setErrorReply("username and pass required");
     }
