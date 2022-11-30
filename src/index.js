12c8282ea93cefefc5653098df29edba1da17d1a
Parker Harris <rparkerharris@gmail.com>
2022-11-29 17:28:54 -0800

    auto dev

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ fetch("https://graffiti.red/API/", {
     method: "POST",
     body: JSON.stringify({
         action: "logOut",
-        username: "mr poopface",
-        token: "j1%Y&yqUAlR9VYNG6DRl%6eXYUGG!WXk7jKfrZhhfwEMXGse7jP84hQGGandv(VlmRXoL",
+        username: "mr poopface"
     })
 }).then((res) => {
 
