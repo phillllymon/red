7ce5086a263f72f9f1b651beb69407d9ab09428d
Parker Harris <rparkerharris@gmail.com>
2022-11-29 17:35:26 -0800

    auto dev

--- API/actions/logOut.php
+++ API/actions/logOut.php
@@ -22,7 +22,7 @@ function logOut($connection, $inputs) {
         return setErrorReply("user not found");
     }
 
-    return $existingUsers[0]->token;
+    return $existingUsers;
 
     if ($existingUsers[0]->token != $inputs->token) {
         return setErrorReply("token invalid");
