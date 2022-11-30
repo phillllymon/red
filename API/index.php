6509eb94fca1fa36aa0d777d49a2ad560c66c308
Parker Harris <rparkerharris@gmail.com>
2022-11-29 16:42:40 -0800

    auto dev

--- API/index.php
+++ API/index.php
@@ -3,7 +3,7 @@
 header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
 
-require "helpers/sanitize.php";
+// require "helpers/sanitize.php";
 require "actions.php";
 
 // TEST ONLY
